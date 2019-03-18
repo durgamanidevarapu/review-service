e935da4ba217259afb71400cf51ea5e3fe0d9fbd
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-18 19:46:45 +0530

    comitting review-service project

---
+++ src/main/java/com/product/review/reviewservice/ReviewServiceApplication.java
@@ -0,0 +1,17 @@
+package com.product.review.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/product/review/reviewservice/service/ReviewService.java
@@ -0,0 +1,14 @@
+package com.product.review.reviewservice.service;
+
+import com.product.review.reviewservice.domain.Review;
+
+import java.util.Optional;
+
+public interface ReviewService {
+
+    public Review addProdcutReview(Review review);
+    public Iterable<Review>  findProductReviews(int product_id);
+    public Review updateproductReview(Review review);
+    public Optional<Review> findProductReview(Long id) ;
+
+}
