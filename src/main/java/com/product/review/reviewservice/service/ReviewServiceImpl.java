95c4d83758c916117ac49b91ba3847634699a595
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-18 19:46:45 +0530

    comitting review-service project

---
+++ src/main/java/com/product/review/reviewservice/service/ReviewServiceImpl.java
@@ -0,0 +1,37 @@
+package com.product.review.reviewservice.service;
+
+import com.product.review.reviewservice.dao.ReviewServiceDao;
+import com.product.review.reviewservice.domain.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    @Autowired
+    ReviewServiceDao reviewServiceDao;
+
+    @Override
+    public Review addProdcutReview(Review review) {
+        return reviewServiceDao.save(review);
+    }
+
+    @Override
+    public Iterable<Review> findProductReviews(int product_id) {
+
+        return null;
+    }
+
+    @Override
+    public Review updateproductReview(Review review) {
+
+        return reviewServiceDao.save(review);
+    }
+
+    @Override
+    public Optional<Review> findProductReview(Long id) {
+        return reviewServiceDao.findById(id);
+
+    }
+}
