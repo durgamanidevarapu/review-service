ecc5e332c9430f6ec02b66517915f7f1be0accb1
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-18 19:47:33 +0530

    comitting review-service project

---
+++ src/main/java/com/product/review/reviewservice/domain/ReviewDto.java
@@ -0,0 +1,25 @@
+package com.product.review.reviewservice.domain;
+
+public class ReviewDto {
+
+
+    private String description;
+    private String rating;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+        public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
