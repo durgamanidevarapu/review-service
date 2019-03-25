bada4e7e241f358994b528dd953dec8e63b947ae
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-25 16:57:19 +0530

    registering review-service to eureka

--- src/main/java/com/product/review/reviewservice/ReviewServiceApplication.java
+++ src/main/java/com/product/review/reviewservice/ReviewServiceApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EntityScan
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
